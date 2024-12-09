6e9b2943146ac9acb4957ab28de4303aed6165dd
Takhir <evloev.taha@mail.ru>
2024-12-09 14:01:33 +0500

    complete project without css

---
+++ src/main/java/org/example/testlab5/controller/TaskController.java
@@ -0,0 +1,12 @@
+//package org.example.testlab5.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class TaskController {
+//    @GetMapping("/create-task")
+//    public String formCreateUser(){
+//        return "add-task";
+//    }
+//}
